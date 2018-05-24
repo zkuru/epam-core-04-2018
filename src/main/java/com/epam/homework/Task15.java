1b7f46b4aec8e078291978d9574ac15072ce219c
Raya A <r.abuzyarova@gmail.com>
2018-05-25 00:04:55 +0300

    Task 15

--- src/main/java/com/epam/homework/Task15.java
+++ src/main/java/com/epam/homework/Task15.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task15 {
 
     /**
@@ -7,27 +9,60 @@ public class Task15 {
      * В случае, если в строке нет двух положительных элементов - сумма от этой строки полагается равной нулю.
      * Сумма между двумя рядом расположенными элементами также равна нулю.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * Целое число, представляющее вычисленную сумму
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
-     *  3
-     *  1   0   3
+     * 3
+     * 1   0   3
      * -1   2   2
-     *  1  -1   3
-     *
+     * 1  -1   3
+     * <p>
      * Выходные данные:
      * -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = readMatrix(scanner);
+
+        int totalSum = 0;
+        for (int[] row : matrix) {
+            totalSum += getSumInRow(row);
+        }
+
+        System.out.println(totalSum);
+    }
+
+    private static int getSumInRow(int[] row) {
+        int count = 0;
+        int sumInRow = 0;
+        for (int element : row) {
+            if (element <= 0 & count > 0) {
+                sumInRow += element;
+            } else if (++count == 2) {
+                break;
+            }
+        }
+        return (count < 2) ? 0 : sumInRow;
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; row++) {
+            for (int column = 0; column < dimension; column++) {
+                matrix[row][column] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
